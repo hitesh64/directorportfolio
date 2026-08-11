aa8a11745faf7ec352108677f13c51712d19b756
Hitesh Ingale <sales@snehprecast.in>
2026-08-11 02:32:03 -0700

    Clean up temporary files

--- push_to_github.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Pushing portfolio to GitHub repository...
-"C:\Users\Hitesh ingale\AppData\Local\Programs\Git\cmd\git.exe" status
-"C:\Users\Hitesh ingale\AppData\Local\Programs\Git\cmd\git.exe" remote set-url origin https://github.com/hitesh64/directorportfolio.git
-"C:\Users\Hitesh ingale\AppData\Local\Programs\Git\cmd\git.exe" push -u origin main --force
